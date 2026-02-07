cdc14dd696021d9e1413323001c0fb6288919aef
Ganesh M <144875358+rohit45-cell@users.noreply.github.com>
2026-02-07 23:22:08 +0530

    Update WSGI configuration for ExpenseTracker
    Removed docstring and added path configuration for WSGI.

--- expensetrack/wsgi.py
+++ expensetrack/wsgi.py
@@ -1,16 +1,11 @@
-"""
-WSGI config for expensetrack project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+path = '/home/Ganesh45/ExpenseTracker'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'expensetrack.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'expensetrack.settings')
-
 application = get_wsgi_application()
